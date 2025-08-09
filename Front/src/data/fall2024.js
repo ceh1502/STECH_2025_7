4fc7eff9d08458b43c365a0ce66b7f0c9dacbfd9
yves <hyunbin0525@naver.com>
2025-08-09 17:38:02 +0900

    2024 fall data

---
+++ Front/src/data/fall2024.js
@@ -0,0 +1,93 @@
+export const FALL_2024_DATA = {
+  season: "2024",
+  event: "Fall",
+  divisions: [
+    {
+      name: "Division 1",
+      groups: [
+        {
+          name: "Group 1",
+          teams: [
+            "Yonsei Eagles",
+            "Hanyang Lions",
+            "HUFS Black Knights",
+            "Korea University Tigers",
+          ],
+          matches: [
+            { stage: "Group", date: "", location: "", home: "Yonsei Eagles", away: "Korea University Tigers", homeScore: 36, awayScore: 0, winner: "Yonsei Eagles" },
+            { stage: "Group", date: "", location: "", home: "Yonsei Eagles", away: "HUFS Black Knights", homeScore: 41, awayScore: 0, winner: "Yonsei Eagles" },
+            { stage: "Group", date: "", location: "", home: "Yonsei Eagles", away: "Hanyang Lions", homeScore: 14, awayScore: 12, winner: "Yonsei Eagles" },
+            { stage: "Group", date: "", location: "", home: "Hanyang Lions", away: "HUFS Black Knights", homeScore: 27, awayScore: 6, winner: "Hanyang Lions" },
+            { stage: "Group", date: "", location: "", home: "Hanyang Lions", away: "Korea University Tigers", homeScore: 13, awayScore: 9, winner: "Hanyang Lions" },
+            { stage: "Group", date: "", location: "", home: "HUFS Black Knights", away: "Korea University Tigers", homeScore: 13, awayScore: 12, winner: "HUFS Black Knights" },
+          ],
+        },
+        {
+          name: "Group 2",
+          teams: [
+            "SNU Green Terrors",
+            "Kookmin Eagles",
+            "UOS City Hawks",
+            "Dongguk Tuskers",
+          ],
+          matches: [
+            { stage: "Group", date: "", location: "", home: "SNU Green Terrors", away: "UOS City Hawks", homeScore: 38, awayScore: 7, winner: "SNU Green Terrors" },
+            { stage: "Group", date: "", location: "", home: "SNU Green Terrors", away: "Dongguk Tuskers", homeScore: 41, awayScore: 6, winner: "SNU Green Terrors" },
+            { stage: "Group", date: "", location: "", home: "SNU Green Terrors", away: "Kookmin Eagles", homeScore: 27, awayScore: 13, winner: "SNU Green Terrors" },
+            { stage: "Group", date: "", location: "", home: "Kookmin Eagles", away: "Dongguk Tuskers", homeScore: 36, awayScore: 0, winner: "Kookmin Eagles" },
+            { stage: "Group", date: "", location: "", home: "Kookmin Eagles", away: "UOS City Hawks", homeScore: 20, awayScore: 0, winner: "Kookmin Eagles" },
+            { stage: "Group", date: "", location: "", home: "UOS City Hawks", away: "Dongguk Tuskers", homeScore: 21, awayScore: 19, winner: "UOS City Hawks" },
+          ],
+        },
+      ],
+      playoffs: [
+        { stage: "Final", date: "", location: "", home: "SNU Green Terrors", away: "Yonsei Eagles", homeScore: 7, awayScore: 19, winner: "Yonsei Eagles" },
+        { stage: "3rd Place", date: "", location: "", home: "Kookmin Eagles", away: "Hanyang Lions", homeScore: 13, awayScore: 22, winner: "Hanyang Lions" },
+        { stage: "5th Place", date: "", location: "", home: "UOS City Hawks", away: "HUFS Black Knights", homeScore: 16, awayScore: 6, winner: "UOS City Hawks" },
+        { stage: "7th Place", date: "", location: "", home: "Dongguk Tuskers", away: "Korea University Tigers", homeScore: 8, awayScore: 10, winner: "Korea University Tigers" },
+      ],
+      promotionRelegation: [
+        { stage: "Promotion/Relegation", date: "", location: "", home: "Korea University Tigers", away: "Hongik Cowboys", homeScore: 6, awayScore: 36, winner: "Hongik Cowboys" },
+      ],
+    },
+    {
+      name: "Division 2",
+      groups: [
+        {
+          name: "Group 1",
+          teams: [
+            "Konkuk Raging Bulls",
+            "Soongsil Crusaders",
+            "Kyunghee Commanders",
+          ],
+          matches: [
+            { stage: "Group", date: "", location: "", home: "Konkuk Raging Bulls", away: "Kyunghee Commanders", homeScore: 35, awayScore: 13, winner: "Konkuk Raging Bulls" },
+            { stage: "Group", date: "", location: "", home: "Konkuk Raging Bulls", away: "Soongsil Crusaders", homeScore: 40, awayScore: 0, winner: "Konkuk Raging Bulls" },
+            { stage: "Group", date: "", location: "", home: "Soongsil Crusaders", away: "Kyunghee Commanders", homeScore: 32, awayScore: 6, winner: "Soongsil Crusaders" },
+          ],
+        },
+        {
+          name: "Group 2",
+          teams: [
+            "Hongik Cowboys",
+            "ChungAng Blue Dragons",
+            "Sogang Albatross",
+          ],
+          matches: [
+            { stage: "Group", date: "", location: "", home: "Hongik Cowboys", away: "ChungAng Blue Dragons", homeScore: 13, awayScore: 6, winner: "Hongik Cowboys" },
+            { stage: "Group", date: "", location: "", home: "Hongik Cowboys", away: "Sogang Albatross", homeScore: 34, awayScore: 7, winner: "Hongik Cowboys" },
+            { stage: "Group", date: "", location: "", home: "ChungAng Blue Dragons", away: "Sogang Albatross", homeScore: 20, awayScore: 6, winner: "ChungAng Blue Dragons" },
+          ],
+        },
+      ],
+      playoffs: [
+        { stage: "Division 2 Final", date: "", location: "", home: "Hongik Cowboys", away: "Konkuk Raging Bulls", homeScore: 8, awayScore: 25, winner: "Konkuk Raging Bulls" },
+        { stage: "Division 2 3rd Place", date: "", location: "", home: "ChungAng Blue Dragons", away: "Soongsil Crusaders", homeScore: 6, awayScore: 22, winner: "Soongsil Crusaders" },
+        { stage: "Division 2 5th Place", date: "", location: "", home: "Sogang Albatross", away: "Kyunghee Commanders", status: "Forfeit", homeScore: null, awayScore: null, winner: "Kyunghee Commanders" },
+      ],
+      promotionRelegation: [
+        { stage: "Promotion/Relegation", date: "", location: "", home: "Korea University Tigers", away: "Hongik Cowboys", homeScore: 6, awayScore: 36, winner: "Hongik Cowboys" },
+      ],
+    },
+  ],
+};
