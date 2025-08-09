7b5d511fead2e76273b0000d97dde69aa2812e33
yves <hyunbin0525@naver.com>
2025-08-09 18:24:11 +0900

    guestStatLayout 수정

--- Front/src/pages/Service/Guest/GuestStat/GuestStatLayout/index.js
+++ Front/src/pages/Service/Guest/GuestStat/GuestStatLayout/index.js
@@ -3,8 +3,6 @@ import { Outlet } from 'react-router-dom';
 const GuestStatLayout = () => {
     return (
         <div>
-            <h1>Guest Stat Page</h1>
-            <p>This is the Guest Stat page content.</p>
             <Outlet />
         </div>
     );
