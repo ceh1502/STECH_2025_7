0067c860cc189fbd8e55f626ab6d582ce6fd420e
Jenicoon <dldnd123@gmail.com>
2025-08-10 23:18:08 +0900

    Add files via upload

---
+++ Front/src/pages/Landing/Docs/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../Header";
+
+const Docs = () => {
+  return(
+    <div>
+      <Header/>
+      <div>
+        
+      </div>  
+    </div>
+  )
+}
+
+export default Docs;
