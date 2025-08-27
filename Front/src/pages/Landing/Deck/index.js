092d1b7fea244c564ce17de3bf1ad052d3c7b2a8
wannahappyaroundme <bu5119@hanyang.ac.kr>
2025-08-28 01:09:14 +0900

    add react-pdf

--- Front/src/pages/Landing/Deck/index.js
+++ Front/src/pages/Landing/Deck/index.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../LandingHome/Header';
 import Footer from '../LandingHome/Footer';
 import './deck.css';
 import TeamLogo from '../../../assets/images/png/TeamPng/teamLogo.png';
-import StechDeck from '../../../assets/images/png/DeckPng/StechDeck.png';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+// PDF.js worker 설정
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Deck = () => {
+    const [numPages, setNumPages] = useState(null);
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+
     return (
         <div className="deckContainer">
             <Header style={{ zIndex: '2' }} />
+
             <div className="deckheader">
                 <div className="decklogoandtitle">
                     <img src={TeamLogo} alt="Stech Logo" className="deckheaderlogo" />
                     <h1 className="decktitle">IR Deck</h1>
                 </div>
             </div>
+
+            {/* PDF 미리보기 */}
             <div className="deckmain">
-                <img src={StechDeck} alt="Stech IR Deck Main" className="deckmainimage" />
-            </div>
-            <div className="deckmessage">
-                <p>
-                    context
-                </p>
+                <div className="pdfViewer">
+                    <Document file="https://drive.google.com/file/d/1QXSPQIC6sH5josNck70QsrsP_UOJP2EM/view?usp=sharing" onLoadSuccess={onDocumentLoadSuccess}>
+                        {Array.from(new Array(numPages), (el, index) => (
+                            <Page key={`page_${index + 1}`} pageNumber={index + 1} width={900} />
+                        ))}
+                    </Document>
+                </div>
             </div>
 
-            <Footer/>
+            <div className="deckmessage">
+                <p>context</p>
+            </div>
+
+            <Footer />
         </div>
     );
 };
